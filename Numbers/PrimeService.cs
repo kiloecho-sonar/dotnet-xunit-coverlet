41a9a71952d8d5b4b890ca8d1ba112f8a572f734
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2022-07-08 16:02:16 -0500

    Add issues

--- Numbers/PrimeService.cs
+++ Numbers/PrimeService.cs
@@ -1,9 +1,23 @@
-﻿namespace System.Numbers
+﻿using System.Security.Cryptography;
+
+namespace System.Numbers
 {
     public class PrimeService
     {
         public bool IsPrime(int candidate)
         {
+            var tripleDES1 = new TripleDESCryptoServiceProvider(); // Noncompliant: Triple DES is vulnerable to meet-in-the-middle attack
+            var simpleDES = new DESCryptoServiceProvider(); // Noncompliant: DES works with 56-bit keys allow attacks via exhaustive search
+            var RC2 = new RC2CryptoServiceProvider(); // Noncompliant: RC2 is vulnerable to a related-key attack
+
+            if (1 < 3)
+            {
+                if (2 < 4)
+                {
+                    // ...
+                }
+            }
+
             if (candidate < 2)
             {
                 return false;
